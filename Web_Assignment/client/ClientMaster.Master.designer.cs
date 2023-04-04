676112ab5abf004331ddc632d1d63f453f7fa385
HinYong1031 <hinyong16@gmail.com>
2023-04-05 01:59:22 +0800

    cart

--- Web_Assignment/client/ClientMaster.Master.designer.cs
+++ Web_Assignment/client/ClientMaster.Master.designer.cs
@@ -49,5 +49,14 @@ namespace Web_Assignment.client
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder GameLibrary;
+
+        /// <summary>
+        /// cart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cart;
     }
 }
