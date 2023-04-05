bc5d443c2250fcbd8e100e95376010fc1df88054
Andrew Hew Jun Sheng <andrew2645401@gmail.com>
2023-04-05 17:57:20 +0800

    update

--- Web_Assignment/client/ClientMaster.Master.designer.cs
+++ Web_Assignment/client/ClientMaster.Master.designer.cs
@@ -33,6 +33,24 @@ namespace Web_Assignment.client
         protected global::System.Web.UI.HtmlControls.HtmlForm form2;
 
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+
+        /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+
+        /// <summary>
         /// Payment control.
         /// </summary>
         /// <remarks>
